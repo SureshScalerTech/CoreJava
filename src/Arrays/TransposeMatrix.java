18595cc18fc9f235ea17c81be6c13ab6539da2df
SureshScalerTech <sureshkarakam123@gmail.com>
2024-12-25 10:45:30 +0530

    TransposeMatrix

---
+++ src/Arrays/TransposeMatrix.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int[] row : transpose) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
